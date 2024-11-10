6f5e5bd2e7c5c1e21f001137ece75ae04df9b5d1
Nana-John <alessandramairis@gmail.com>
2024-11-10 20:23:01 -0300

    finalizacao

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,20 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
-
+async function image() {
+    const imagemin = await import('gulp-imagemin');
+    return gulp.src('./src/image/**/*')
+        .pipe(imagemin.default())
+        .pipe(gulp.dest('./dist/image'));
+}
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
-        .pipe(sass({ outputStyle: 'compressed'}))
+        .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(gulp.dest('./dist/css'));
 }
-exports.default = styles;
+
+exports.default = gulp.parallel(styles, image);
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
