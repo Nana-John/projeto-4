c381a6220bee49f32895d5750fdb2bfd32d73692
Nana-John <alessandramairis@gmail.com>
2024-11-11 00:51:21 -0300

    ajuste de imagemin e uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,30 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
 
-async function image() {
-    const imagemin = await import('gulp-imagemin');
-    return gulp.src('./src/image/**/*')
-        .pipe(imagemin.default())
-        .pipe(gulp.dest('./dist/image'));
+
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'));
 }
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
-        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(sass({ outputStyle: 'compressed'}))
         .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = gulp.parallel(styles, image);
+function images() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
+}
+
+exports.default = gulp.parallel(styles, images, scripts);
+
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
